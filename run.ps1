d0a153406724edbdfdbd8f9b194a6a3993639bd4
Nikolche Kolev <nikolce1kolev@gmail.com>
2026-02-02 12:57:37 -0800

    clarify

--- run.ps1
+++ run.ps1
@@ -1,4 +1,6 @@
+Write-Host "Starting clean and restore process..."
 git clean -xdf
 dotnet restore
+Write-Host "Starting clean and restore process with --source option..."
 git clean -xdf
 dotnet restore --source "http://api.nuget.org/v3/index.json"
